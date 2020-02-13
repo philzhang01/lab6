629ffaad624431653405756551bc4b3031f05c02
philzhang01 <philzhang01@gmail.com>
2020-02-12 22:47:04 -0800

    ajax and facebook lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -1,30 +1,46 @@
-'use strict';
+"use strict";
 
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
-	initializePage();
-})
+  initializePage();
+});
 
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	$('.project a').click(addProjectDetails);
+  $(".project a").click(addProjectDetails);
 
-	// $('#colorBtn').click(randomizeColors);
+  // $('#colorBtn').click(randomizeColors);
 }
 
 /*
  * Make an AJAX call to retrieve project details and add it in
  */
 function addProjectDetails(e) {
-	// Prevent following the link
-	e.preventDefault();
+  // Prevent following the link
+  e.preventDefault();
 
-	// Get the div ID, e.g., "project3"
-	var projectID = $(this).closest('.project').attr('id');
-	// get rid of 'project' from the front of the id 'project3'
-	var idNumber = projectID.substr('project'.length);
+  // Get the div ID, e.g., "project3"
+  var projectID = $(this)
+    .closest(".project")
+    .attr("id");
+  // get rid of 'project' from the front of the id 'project3'
+  var idNumber = projectID.substr("project".length);
 
-	console.log("User clicked on project " + idNumber);
+  console.log("User clicked on project " + idNumber);
+  console.log("/project/" + idNumber);
+
+  $.get("/project/" + idNumber, function(result) {
+    var project =
+      '<img src="' +
+      result["image"] +
+      '" class="detailsImage"><p>' +
+      result["title"] +
+      "</p><p><small>" +
+      result["date"] +
+      "</small></p>";
+
+    $("#project" + idNumber + "  .details").html(project + result.summary);
+  });
 }
